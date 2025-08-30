2a51a4c6342effb9c3b39efb7d185d32588209f7
majid4360 <majid4360@gmail.com>
2025-08-30 12:41:06 +0330

    chore: add adittions

---
+++ src/pages/CaseStudy.jsx
@@ -0,0 +1,3 @@
+export default function CaseStudy() {
+  return <h1>About Page</h1>;
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,3 @@
-// src/pages/Home.jsx
 export default function Home() {
-  return (
-    <section className="py-16">
-      <h1 className="text-4xl md:text-6xl font-semibold">
-        Web designer crafting clean, conversion‑focused interfaces.
-      </h1>
-      <p className="mt-4 text-lg text-gray-600">
-        I design and build interfaces that feel effortless. See selected work below.
-      </p>
-    </section>
-  );
+  return <h1>Home Page</h1>;
 }
